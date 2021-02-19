e8927a0db49e9ebd16c06ae8d2426e36a3ad57e2
SaiKrishna1545 <s538340@nwmissouri.edu>
2021-02-19 15:56:34 -0600

    updated

---
+++ src/main/java/edu/nwmissouri/sai/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.sai;
+
+/**
+ * This is begins executions of my app
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
